928ada4476e712b60bc629b73226b6051a420fe6
Adam Walmsley <adam.walmsley0123@gmail.com>
2025-01-21 13:05:34 -0800

    blah

---
+++ script.sh
@@ -0,0 +1,18 @@
+# bashscript.sh
+
+# git switch  -c <name> will create a new branch and switch to it
+
+echo "my second branch"
+
+# Mona Lisa ASCII Art
+cat << "EOF"
+        _____
+     .-'.  ':'-.
+    /:  .'\   /
+   ;:.   .'-' 
+    \::.   '. 
+     '::'--'
+      ||
+      ||
+    __||__
+   '------'
